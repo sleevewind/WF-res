58e90b0446f8d462711968bcdfb8f571d5e617ec
sleevewind <34911610+sleevewind@users.noreply.github.com>
2026-05-07 17:36:54 +0800

    Add files via upload

---
+++ object_detection/mmcv_custom/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from .checkpoint import load_checkpoint
+
+__all__ = ['load_checkpoint', 'LearningRateDecayOptimizerConstructor', 'CustomizedTextLoggerHook']
+from .checkpoint import load_checkpoint
+from .layer_decay_optimizer_constructor import LearningRateDecayOptimizerConstructor
+from .customized_text import CustomizedTextLoggerHook
